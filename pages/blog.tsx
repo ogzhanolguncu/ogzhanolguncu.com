3048871808ed41273868994536b72b64b7206338
Oguzhan Olguncu <ogzhan11@gmail.com>
2020-10-08 23:58:18 +0300

    added group by year to blog page

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,5 +1,5 @@
 import { GetStaticProps } from 'next';
-import React, { useContext, useEffect, useState } from 'react';
+import React, { Fragment, useContext, useState } from 'react';
 import Link from 'next/link';
 import { getSortedPostsData } from 'lib/posts';
 import { StaticBlog } from 'global';
@@ -11,38 +11,38 @@ import {
   useColorMode,
   Link as StyledLink,
   Text,
+  Divider,
   Input,
 } from '@chakra-ui/core';
 import colorMap from 'styles/colorMap';
 import { addTwoMonthToPublishedDate, compareDateWithTodaysDate } from 'utils/dateOperations';
 import { ColorModeContext } from '@contexts/CustomColorContext';
 import { Article, ArticleTitle, Layout } from '@components/index';
-// import groupBy from 'lodash.groupby';
+import groupBy from 'lodash.groupby';
+import Fuse from 'fuse.js';
 
 type Props = {
+  groupedBlogPosts: Record<number, StaticBlog[]>;
   blogPosts: StaticBlog[];
-  // grouped: any;
 };
 
-const Blog = ({ blogPosts }: Props) => {
-  // eslint-disable-next-line no-console
-  // console.log(grouped);
+const options = {
+  includeScore: true,
+  keys: ['title', 'id', 'publishedAt'],
+};
+
+const Blog = ({ groupedBlogPosts, blogPosts }: Props) => {
   const colorModeObj = useContext(ColorModeContext);
   const { colorMode } = useColorMode();
+  const [fusedBlog, setFusedBlog] = useState<StaticBlog[]>();
 
-  const [input, setInput] = useState<string>('');
-  const [searchedBlogPosts, setSearchedBlogPosts] = useState<StaticBlog[]>([]);
-
+  const fuse = new Fuse(blogPosts, options);
   const handleChangeBlog = (value: string) => {
-    setInput(value);
+    const result = fuse.search(value);
+    setFusedBlog(result);
+    // eslint-disable-next-line no-console
+    console.log(fusedBlog);
   };
-
-  useEffect(() => {
-    const searchedBlogs = blogPosts.filter((blog) =>
-      blog.title.toLowerCase().includes(input.toLowerCase()),
-    );
-    setSearchedBlogPosts(searchedBlogs);
-  }, [input]);
 
   return (
     <Layout>
@@ -64,96 +64,101 @@ const Blog = ({ blogPosts }: Props) => {
           onChange={(e: React.ChangeEvent<HTMLInputElement>): void => {
             handleChangeBlog(e.target.value);
           }}
-          value={input}
           variant="outline"
           placeholder="Search..."
           maxWidth="400px"
         />
       </Flex>
       <Flex alignItems="flex-start" justifyContent="center" flexDirection="column">
-        {searchedBlogPosts.length > 0 ? (
-          searchedBlogPosts.map((blog) => (
-            <Article
-              key={blog.id}
-              justifyContent="space-between"
-              alignItems="space-between"
-              color={colorMode === 'light' ? 'light' : 'dark'}
-            >
-              <Link href={`/blog/${blog.id}`}>
-                <StyledLink _hover={{ textDecoration: 'none' }}>
-                  <ArticleTitle>
-                    {compareDateWithTodaysDate(addTwoMonthToPublishedDate(blog.publishedAt)) ? (
-                      <Tag
-                        fontSize={['.7rem', '.7rem', '.8rem', '.7 rem']}
-                        p=".5rem"
-                        borderRadius=".3rem"
-                        m={[
-                          'auto .4rem auto 0',
-                          'auto .4rem auto 0',
-                          'auto .4rem auto 0',
-                          '1rem 1rem 10px 0',
-                        ]} //for responsive
-                        height="15px"
-                        backgroundColor="#d3f9d8"
-                        fontWeight="700"
-                        width={['2.7rem', '2.7rem', '', '']}
-                        minW=""
-                        color={colorModeObj.articleNewTagTextColor[colorMode]}
-                        background={colorModeObj.articleNewTagBackgroundColor[colorMode]}
-                      >
-                        New!
-                      </Tag>
-                    ) : null}
-                    <Box>
-                      <Text color="#787f87" fontSize=".8rem" fontWeight="600">
-                        {blog.publishedAt}
-                      </Text>
-                      <Heading
-                        fontSize={['1rem', '1.1rem', '1.15rem', '1.15rem']}
-                        w={['100%', '100%', 'max-content', 'max-content']}
-                      >
-                        {blog.title}
-                      </Heading>
-                    </Box>
-                  </ArticleTitle>
-                </StyledLink>
-              </Link>
-              <Box
-                d="flex"
-                flexDirection={['column', 'row', 'row', 'row']}
-                justifyContent={['flex-start', 'flex-start', 'flex-end', 'flex-end']}
-                alignItems={['flex-start', 'center', 'center', 'center']}
-                w="100%"
-                flexWrap="wrap"
-              >
-                {blog?.languageTags?.map((tag, index) => {
-                  const color = colorMap[tag.toLowerCase()];
-                  return (
-                    <Tag
-                      key={index}
-                      width="max-content"
-                      height="20px"
-                      p=".3rem .5rem"
-                      fontSize=".8rem"
-                      borderRadius="16px"
-                      marginBottom="7px"
-                      marginRight=".5rem"
-                      color="#fff"
-                      backgroundColor={color?.color}
-                      _hover={{ cursor: 'pointer', backgroundColor: color?.hover }}
-                    >
-                      {tag}
-                    </Tag>
-                  );
-                })}
-              </Box>
-            </Article>
-          ))
-        ) : (
-          <Text textAlign="center" fontSize="1rem" color="#60656c">
-            Sorry, nothing matched that search.
-          </Text>
-        )}
+        {Object.keys(groupedBlogPosts)
+          .reverse()
+          .map((blog, index) => (
+            <Fragment key={index}>
+              <Heading size="lg" marginTop={index !== 0 ? '4rem' : '0'}>
+                {blog}
+              </Heading>
+              <Divider />
+              {groupedBlogPosts[(blog as unknown) as number].map((article) => (
+                <Article
+                  key={`${blog}${article.id}`}
+                  justifyContent="space-between"
+                  alignItems="space-between"
+                  color={colorMode === 'light' ? 'light' : 'dark'}
+                >
+                  <Link href={`/blog/${article.id}`}>
+                    <StyledLink _hover={{ textDecoration: 'none' }}>
+                      <ArticleTitle>
+                        {compareDateWithTodaysDate(
+                          addTwoMonthToPublishedDate(article.publishedAt),
+                        ) ? (
+                          <Tag
+                            fontSize={['.7rem', '.7rem', '.8rem', '.7 rem']}
+                            p=".5rem"
+                            borderRadius=".3rem"
+                            m={[
+                              'auto .4rem auto 0',
+                              'auto .4rem auto 0',
+                              'auto .4rem auto 0',
+                              '1rem 1rem 10px 0',
+                            ]} //for responsive
+                            height="15px"
+                            backgroundColor="#d3f9d8"
+                            fontWeight="700"
+                            width={['2.7rem', '2.7rem', '', '']}
+                            minW=""
+                            color={colorModeObj.articleNewTagTextColor[colorMode]}
+                            background={colorModeObj.articleNewTagBackgroundColor[colorMode]}
+                          >
+                            New!
+                          </Tag>
+                        ) : null}
+                        <Box>
+                          <Text color="#787f87" fontSize=".8rem" fontWeight="600">
+                            {article.publishedAt}
+                          </Text>
+                          <Heading
+                            fontSize={['1rem', '1.1rem', '1.15rem', '1.15rem']}
+                            w={['100%', '100%', 'max-content', 'max-content']}
+                          >
+                            {article.title}
+                          </Heading>
+                        </Box>
+                      </ArticleTitle>
+                    </StyledLink>
+                  </Link>
+                  <Box
+                    d="flex"
+                    flexDirection={['column', 'row', 'row', 'row']}
+                    justifyContent={['flex-start', 'flex-start', 'flex-end', 'flex-end']}
+                    alignItems={['flex-start', 'center', 'center', 'center']}
+                    w="100%"
+                    flexWrap="wrap"
+                  >
+                    {article?.languageTags?.map((tag, tagIndex) => {
+                      const color = colorMap[tag.toLowerCase()];
+                      return (
+                        <Tag
+                          key={`${blog}${article.id}${tagIndex}`}
+                          width="max-content"
+                          height="20px"
+                          p=".3rem .5rem"
+                          fontSize=".8rem"
+                          borderRadius="16px"
+                          marginBottom="7px"
+                          marginRight=".5rem"
+                          color="#fff"
+                          backgroundColor={color?.color}
+                          _hover={{ cursor: 'pointer', backgroundColor: color?.hover }}
+                        >
+                          {tag}
+                        </Tag>
+                      );
+                    })}
+                  </Box>
+                </Article>
+              ))}
+            </Fragment>
+          ))}
       </Flex>
     </Layout>
   );
@@ -163,9 +168,9 @@ export default Blog;
 
 export const getStaticProps: GetStaticProps = async () => {
   const blogPosts = getSortedPostsData();
-  //Slicing used to get first four digit of date => YYYY-DD-MM
-  // const grouped = groupBy(blogPosts, (x) => x.publishedAt.toString().slice(0, 4));
+  // Slicing used to get first four digit of date => YYYY-DD-MM
+  const groupedBlogPosts = groupBy(blogPosts, (x) => x.publishedAt.toString().slice(0, 4));
   return {
-    props: { blogPosts },
+    props: { groupedBlogPosts, blogPosts },
   };
 };
