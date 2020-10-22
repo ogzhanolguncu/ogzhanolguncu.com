81dbf02019f188635b02a79c53a6356551017290
Oguzhan Olguncu <ogzhan11@gmail.com>
2020-10-23 02:00:27 +0300

    meta tags added for seo

--- pages/about.tsx
+++ pages/about.tsx
@@ -4,188 +4,202 @@ import { Layout } from '@components/index';
 import TextLink from '@components/TextLink';
 import { useContext } from 'react';
 import { ColorModeContext } from '@contexts/CustomColorContext';
+import { NextSeo } from 'next-seo';
+
+const url = 'https://ogzhanolguncu.com/about';
+const title = 'About Me – Oğuzhan Olguncu';
 
 const About = () => {
   const colorModeObj = useContext(ColorModeContext);
   const { colorMode } = useColorMode();
 
   return (
-    <Layout>
-      <Flex
-        flexDirection="column"
-        justifyContent="center"
-        alignItems="flex-start"
-        w="840px"
-        maxW="100%"
-        mx="auto"
-        p="0 2rem"
-      >
-        <Heading
-          textAlign="center"
-          fontSize={['2rem', '2rem', '3rem', '3rem']}
-          color={
-            colorMode === 'light' ? colorModeObj.titleColor.light : colorModeObj.titleColor.dark
-          }
-          margin="5rem auto"
+    <>
+      <NextSeo
+        title={title}
+        canonical={url}
+        openGraph={{
+          url,
+          title,
+        }}
+      />
+      <Layout>
+        <Flex
+          flexDirection="column"
+          justifyContent="center"
+          alignItems="flex-start"
+          w="840px"
+          maxW="100%"
+          mx="auto"
+          p="0 2rem"
         >
-          About me
-        </Heading>
-        <Text
-          fontSize="1.05rem"
-          mb="1.5rem"
-          fontWeight="400"
-          color={
-            colorMode === 'light'
-              ? colorModeObj.aboutTextColor.light
-              : colorModeObj.aboutTextColor.dark
-          }
-        >
-          Hi! I'm Oğuzhan Olguncu. I currently work full-time as a Frontend Developer, and I build
-          {''}
-          <TextLink text=" open-source projects " url="#" /> for fun.
-        </Text>
-        <Text
-          fontSize="1.05rem"
-          mb="1.5rem"
-          fontWeight="400"
-          color={
-            colorMode === 'light'
-              ? colorModeObj.aboutTextColor.light
-              : colorModeObj.aboutTextColor.dark
-          }
-        >
-          I taught myself how to make websites first as a hobby and later as a professional
-          programmer. I've been documenting everything I learn since I began.
-        </Text>
-        <Text
-          fontSize="1.05rem"
-          mb="1.5rem"
-          fontWeight="400"
-          color={
-            colorMode === 'light'
-              ? colorModeObj.aboutTextColor.light
-              : colorModeObj.aboutTextColor.dark
-          }
-        >
-          I also like to make open source projects, all of which can be found on{' '}
-          <TextLink text="Github" url="#" />. A few I'm proud of are:
-        </Text>
+          <Heading
+            textAlign="center"
+            fontSize={['2rem', '2rem', '3rem', '3rem']}
+            color={
+              colorMode === 'light' ? colorModeObj.titleColor.light : colorModeObj.titleColor.dark
+            }
+            margin="5rem auto"
+          >
+            About me
+          </Heading>
+          <Text
+            fontSize="1.05rem"
+            mb="1.5rem"
+            fontWeight="400"
+            color={
+              colorMode === 'light'
+                ? colorModeObj.aboutTextColor.light
+                : colorModeObj.aboutTextColor.dark
+            }
+          >
+            Hi! I'm Oğuzhan Olguncu. I currently work full-time as a Frontend Developer, and I build
+            {''}
+            <TextLink text=" open-source projects " url="#" /> for fun.
+          </Text>
+          <Text
+            fontSize="1.05rem"
+            mb="1.5rem"
+            fontWeight="400"
+            color={
+              colorMode === 'light'
+                ? colorModeObj.aboutTextColor.light
+                : colorModeObj.aboutTextColor.dark
+            }
+          >
+            I taught myself how to make websites first as a hobby and later as a professional
+            programmer. I've been documenting everything I learn since I began.
+          </Text>
+          <Text
+            fontSize="1.05rem"
+            mb="1.5rem"
+            fontWeight="400"
+            color={
+              colorMode === 'light'
+                ? colorModeObj.aboutTextColor.light
+                : colorModeObj.aboutTextColor.dark
+            }
+          >
+            I also like to make open source projects, all of which can be found on{' '}
+            <TextLink text="Github" url="#" />. A few I'm proud of are:
+          </Text>
 
-        <Text
-          fontSize="1.05rem"
-          mb="1.5rem"
-          fontWeight="400"
-          color={
-            colorMode === 'light'
-              ? colorModeObj.aboutTextColor.light
-              : colorModeObj.aboutTextColor.dark
-          }
-        >
-          I also like classical turkish music and powerlifting.
-        </Text>
-        <Heading
-          fontSize="1.6rem"
-          color={
-            colorMode === 'light' ? colorModeObj.titleColor.light : colorModeObj.titleColor.dark
-          }
-          mb="1rem"
-        >
-          Get in touch
-        </Heading>
-        <Text
-          fontSize="1.05rem"
-          mb="1.5rem"
-          fontWeight="400"
-          color={
-            colorMode === 'light'
-              ? colorModeObj.aboutTextColor.light
-              : colorModeObj.aboutTextColor.dark
-          }
-        >
-          I hope you love the site, and if there's anything you want to talk about with me feel free
-          to drop me a line by email. I'm happy to hear your comments, feedback, suggestions, or
-          just say hi!{' '}
-          <em>(Emails regarding ads, sponsored posts, link-sharing, etc. will be ignored.)</em>
-        </Text>
-        <List
-          styleType="disc"
-          fontSize="1.05rem"
-          mb="3rem"
-          p={['0 0', '0 0', '0 2rem', '0 2rem']}
-          color={
-            colorMode === 'light'
-              ? colorModeObj.aboutTextColor.light
-              : colorModeObj.aboutTextColor.dark
-          }
-        >
-          <ListItem>
-            📧 &nbsp; <strong>Github:</strong> {''}
-            <TextLink text="@ogzhanolguncu" url="https://github.com/ogzhanolguncu" />
-          </ListItem>
-          <ListItem>
-            🐙 &nbsp; <strong>Email:</strong> {''}
-            <TextLink text="@ogzhanolguncu" url="mailto:ogzhan11@gmail.com" />
-          </ListItem>
-        </List>
-        <Heading
-          fontSize="1.6rem"
-          color={
-            colorMode === 'light' ? colorModeObj.titleColor.light : colorModeObj.titleColor.dark
-          }
-          mb="1.5rem"
-          pb="1rem"
-          borderBottom="1px solid #d6d9de"
-          w="100%"
-        >
-          Timeline
-        </Heading>
-        <List
-          styleType="disc"
-          fontSize="1.05rem"
-          mb="3rem"
-          p={['0 0', '0 0', '0 2rem', '0 2rem']}
-          color={
-            colorMode === 'light'
-              ? colorModeObj.aboutTextColor.light
-              : colorModeObj.aboutTextColor.dark
-          }
-        >
-          <ListItem>
-            <strong>1989:</strong> Born in Chicago, the youngest of four and the only girl.
-          </ListItem>
-          <ListItem>
-            <strong>1994 – 1997:</strong> Family gets our first computer, a PC running DOS then
-            Windows 3.11, then back to DOS for a while after my brother deleted Win.exe. I use the
-            computer a lot, mostly drawing things on MS Paint and playing WarCraft.
-          </ListItem>
-          <ListItem>
-            <strong>1998 – 2005:</strong> My brother takes me to the library, and I discover the
-            internet. On the first day, I make an email address and learn the meaning of "lol". I
-            draw things at home, save them to floppy disks, and upload them online. I join and
-            create tons of forum throughout the years. I make websites for the guilds I'm part of,
-            made-up Digimon, my art, myself, video games, and anything else I'm into.
-          </ListItem>
-          <ListItem>
-            <strong>2005:</strong> I make a website about Seona Dancing, Ricky Gervais's failed
-            eighties duo musical endeavor. (This seems to be one of the few sites the Internet
-            Archive has retained.)
-          </ListItem>
-          <ListItem>
-            <strong>2007:</strong> I graduate high school. With no specific plans, ideas, or
-            aspirations of what I should do with the rest of my life, I go to college for Culinary
-            Arts.
-          </ListItem>
-          <ListItem>
-            <strong>2017:</strong> After two years, I quit to solo travel around Europe for a couple
-            months, visiting 15 countries while writing for DigitalOcean.
-          </ListItem>
-          <ListItem>
-            <strong>2019 – Present:</strong> Working as a Senior Software Engineer for a large
-            corporation.
-          </ListItem>
-        </List>
-      </Flex>
-    </Layout>
+          <Text
+            fontSize="1.05rem"
+            mb="1.5rem"
+            fontWeight="400"
+            color={
+              colorMode === 'light'
+                ? colorModeObj.aboutTextColor.light
+                : colorModeObj.aboutTextColor.dark
+            }
+          >
+            I also like classical turkish music and powerlifting.
+          </Text>
+          <Heading
+            fontSize="1.6rem"
+            color={
+              colorMode === 'light' ? colorModeObj.titleColor.light : colorModeObj.titleColor.dark
+            }
+            mb="1rem"
+          >
+            Get in touch
+          </Heading>
+          <Text
+            fontSize="1.05rem"
+            mb="1.5rem"
+            fontWeight="400"
+            color={
+              colorMode === 'light'
+                ? colorModeObj.aboutTextColor.light
+                : colorModeObj.aboutTextColor.dark
+            }
+          >
+            I hope you love the site, and if there's anything you want to talk about with me feel
+            free to drop me a line by email. I'm happy to hear your comments, feedback, suggestions,
+            or just say hi!{' '}
+            <em>(Emails regarding ads, sponsored posts, link-sharing, etc. will be ignored.)</em>
+          </Text>
+          <List
+            styleType="disc"
+            fontSize="1.05rem"
+            mb="3rem"
+            p={['0 0', '0 0', '0 2rem', '0 2rem']}
+            color={
+              colorMode === 'light'
+                ? colorModeObj.aboutTextColor.light
+                : colorModeObj.aboutTextColor.dark
+            }
+          >
+            <ListItem>
+              📧 &nbsp; <strong>Github:</strong> {''}
+              <TextLink text="@ogzhanolguncu" url="https://github.com/ogzhanolguncu" />
+            </ListItem>
+            <ListItem>
+              🐙 &nbsp; <strong>Email:</strong> {''}
+              <TextLink text="@ogzhanolguncu" url="mailto:ogzhan11@gmail.com" />
+            </ListItem>
+          </List>
+          <Heading
+            fontSize="1.6rem"
+            color={
+              colorMode === 'light' ? colorModeObj.titleColor.light : colorModeObj.titleColor.dark
+            }
+            mb="1.5rem"
+            pb="1rem"
+            borderBottom="1px solid #d6d9de"
+            w="100%"
+          >
+            Timeline
+          </Heading>
+          <List
+            styleType="disc"
+            fontSize="1.05rem"
+            mb="3rem"
+            p={['0 0', '0 0', '0 2rem', '0 2rem']}
+            color={
+              colorMode === 'light'
+                ? colorModeObj.aboutTextColor.light
+                : colorModeObj.aboutTextColor.dark
+            }
+          >
+            <ListItem>
+              <strong>1989:</strong> Born in Chicago, the youngest of four and the only girl.
+            </ListItem>
+            <ListItem>
+              <strong>1994 – 1997:</strong> Family gets our first computer, a PC running DOS then
+              Windows 3.11, then back to DOS for a while after my brother deleted Win.exe. I use the
+              computer a lot, mostly drawing things on MS Paint and playing WarCraft.
+            </ListItem>
+            <ListItem>
+              <strong>1998 – 2005:</strong> My brother takes me to the library, and I discover the
+              internet. On the first day, I make an email address and learn the meaning of "lol". I
+              draw things at home, save them to floppy disks, and upload them online. I join and
+              create tons of forum throughout the years. I make websites for the guilds I'm part of,
+              made-up Digimon, my art, myself, video games, and anything else I'm into.
+            </ListItem>
+            <ListItem>
+              <strong>2005:</strong> I make a website about Seona Dancing, Ricky Gervais's failed
+              eighties duo musical endeavor. (This seems to be one of the few sites the Internet
+              Archive has retained.)
+            </ListItem>
+            <ListItem>
+              <strong>2007:</strong> I graduate high school. With no specific plans, ideas, or
+              aspirations of what I should do with the rest of my life, I go to college for Culinary
+              Arts.
+            </ListItem>
+            <ListItem>
+              <strong>2017:</strong> After two years, I quit to solo travel around Europe for a
+              couple months, visiting 15 countries while writing for DigitalOcean.
+            </ListItem>
+            <ListItem>
+              <strong>2019 – Present:</strong> Working as a Senior Software Engineer for a large
+              corporation.
+            </ListItem>
+          </List>
+        </Flex>
+      </Layout>
+    </>
   );
 };
 
--- pages/guides.tsx
+++ pages/guides.tsx
@@ -6,68 +6,86 @@ import ExampleImg from 'images/typescript.png';
 import { getSortedPostsData } from 'lib/posts';
 import { GetStaticProps } from 'next';
 import { useContext } from 'react';
+import { NextSeo } from 'next-seo';
 
 type Props = {
   guides: StaticBlog[];
 };
 
+const url = 'https://ogzhanolguncu.com/guides';
+const title = 'Guides – Oğuzhan Olguncu';
+const description =
+  'Guides for all kinds of javascript/typescript tutorials which written from scratch.';
+
 const guides = ({ guides }: Props) => {
   const colorModeObj = useContext(ColorModeContext);
   const { colorMode } = useColorMode();
   return (
-    <Layout>
-      <Flex justifyContent="center" alignItems="center" flexDirection="column" margin="5rem 0">
-        <Heading
-          textAlign="center"
-          as="h2"
-          fontSize={['2rem', '2rem', '3rem', '3rem']}
-          marginBottom="1rem"
-          marginTop={['0.6rem', '0', '0', '0']}
-          fontWeight="bold"
-          color={colorModeObj.titleColor[colorMode]}
-        >
-          Guides
-        </Heading>
-        <Text
-          textAlign="center"
-          fontSize="1.3rem"
-          color={
-            colorMode === 'light' ? colorModeObj.titleColor.light : colorModeObj.titleColor.dark
-          }
-        >
-          The missing instruction manuals of the web.
-        </Text>
-      </Flex>
-      {guides.length >= 1 ? (
-        <Box
-          d="grid"
-          gridTemplateColumns={[
-            'repeat(1,minmax(0,1fr))',
-            'repeat(2,minmax(0,1fr))',
-            'repeat(2,minmax(0,1fr))',
-            'repeat(3,minmax(0,1fr))',
-          ]}
-          gridGap="1.5rem"
-          mb={['3rem', '4rem', '5rem', '5rem']}
-        >
-          {guides.map((blog: StaticBlog, index: number) => {
-            return (
-              <Card
-                key={index}
-                id={blog.id}
-                img={ExampleImg}
-                title={blog.title}
-                description={blog.summary}
-              />
-            );
-          })}
-        </Box>
-      ) : (
-        <Text textAlign="center" fontSize="1.1rem">
-          There will be guides soon.
-        </Text>
-      )}
-    </Layout>
+    <>
+      <NextSeo
+        title={title}
+        description={description}
+        canonical={url}
+        openGraph={{
+          url,
+          title,
+          description,
+        }}
+      />
+      <Layout>
+        <Flex justifyContent="center" alignItems="center" flexDirection="column" margin="5rem 0">
+          <Heading
+            textAlign="center"
+            as="h2"
+            fontSize={['2rem', '2rem', '3rem', '3rem']}
+            marginBottom="1rem"
+            marginTop={['0.6rem', '0', '0', '0']}
+            fontWeight="bold"
+            color={colorModeObj.titleColor[colorMode]}
+          >
+            Guides
+          </Heading>
+          <Text
+            textAlign="center"
+            fontSize="1.3rem"
+            color={
+              colorMode === 'light' ? colorModeObj.titleColor.light : colorModeObj.titleColor.dark
+            }
+          >
+            The missing instruction manuals of the web.
+          </Text>
+        </Flex>
+        {guides.length >= 1 ? (
+          <Box
+            d="grid"
+            gridTemplateColumns={[
+              'repeat(1,minmax(0,1fr))',
+              'repeat(2,minmax(0,1fr))',
+              'repeat(2,minmax(0,1fr))',
+              'repeat(3,minmax(0,1fr))',
+            ]}
+            gridGap="1.5rem"
+            mb={['3rem', '4rem', '5rem', '5rem']}
+          >
+            {guides.map((blog: StaticBlog, index: number) => {
+              return (
+                <Card
+                  key={index}
+                  id={blog.id}
+                  img={ExampleImg}
+                  title={blog.title}
+                  description={blog.summary}
+                />
+              );
+            })}
+          </Box>
+        ) : (
+          <Text textAlign="center" fontSize="1.1rem">
+            There will be guides soon.
+          </Text>
+        )}
+      </Layout>
+    </>
   );
 };
 
