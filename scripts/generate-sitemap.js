e232a7e002405f427424c9f1da09fc46f75d0fab
Oguzhan Olguncu <ogzhan11@gmail.com>
2020-10-30 17:38:23 +0300

    site map fixed

--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -5,13 +5,13 @@ const prettier = require('prettier');
 
 (async () => {
   const prettierConfig = await prettier.resolveConfig('./.prettierrc.js');
-  const pages = await globby(['pages/**/*{.js,.mdx}', '!pages/_*.js', '!pages/api']);
+  const pages = await globby(['pages/**/*{.tsx,.mdx}', '!pages/_*.tsx', '!pages/api']);
   const sitemap = `
         <?xml version="1.0" encoding="UTF-8"?>
         <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
             ${pages
               .map((page) => {
-                const path = page.replace('pages', '').replace('.js', '').replace('.mdx', '');
+                const path = page.replace('pages', '').replace('.tsx', '').replace('.mdx', '');
                 const route = path === '/index' ? '' : path;
                 return `
                         <url>
