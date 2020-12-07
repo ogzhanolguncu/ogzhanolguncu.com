e29efb32753d715f4702d28dabdbe4f287d20920
Oguzhan Olguncu <ogzhan11@gmail.com>
2020-12-07 23:02:00 +0300

    husky blog added

--- components/ProjectItem.tsx
+++ components/ProjectItem.tsx
@@ -1,5 +1,4 @@
 import { Flex, Link as StyledLink, Text, Box } from '@chakra-ui/core';
-import Link from 'next/link';
 
 type Props = {
   colorMode: string;
@@ -11,32 +10,31 @@ type Props = {
 
 const ProjectItems = ({ colorMode, unicodeIcon, leftSide, rightSide, redirectUrl }: Props) => {
   return (
-    <Link href={redirectUrl}>
-      <StyledLink
-        _hover={{
-          textDecoration: 'none',
-          bg: colorMode === 'light' ? '#f6f8fb' : '#10151fbf',
-          transition: 'all 0.1s ease-in',
-        }}
-        m="auto -1rem"
-        borderRadius="20px"
-        padding=".5rem 1rem"
-      >
-        <Flex flexDirection={['column', 'column', 'row', 'row']}>
-          <Flex flex="1">
-            <Box h="10" mr="0.8rem" fontSize="1.15rem">
-              {unicodeIcon}
-            </Box>
-            <Text h="10" fontSize="1.15rem" fontWeight="bold">
-              {leftSide}
-            </Text>
-          </Flex>
-          <Text h="10" flex="3" fontSize="1.1rem" fontWeight="400" color="#787f87">
-            {rightSide}
+    <StyledLink
+      _hover={{
+        textDecoration: 'none',
+        bg: colorMode === 'light' ? '#f6f8fb' : '#10151fbf',
+        transition: 'all 0.1s ease-in',
+      }}
+      m="auto -1rem"
+      borderRadius="20px"
+      padding=".5rem 1rem"
+      href={redirectUrl}
+    >
+      <Flex flexDirection={['column', 'column', 'row', 'row']}>
+        <Flex flex="1">
+          <Box h="10" mr="0.8rem" fontSize="1.15rem">
+            {unicodeIcon}
+          </Box>
+          <Text h="10" fontSize="1.15rem" fontWeight="bold">
+            {leftSide}
           </Text>
         </Flex>
-      </StyledLink>
-    </Link>
+        <Text h="10" flex="3" fontSize="1.1rem" fontWeight="400" color="#787f87">
+          {rightSide}
+        </Text>
+      </Flex>
+    </StyledLink>
   );
 };
 
