6c9aed9bf1c717db0842fa26c016ea8714a2ee3c
ogzhanolguncu <ogzhan11@gmail.com>
2021-01-28 14:08:30 +0300

    added preact to decrease the bundle size

--- next.config.js
+++ next.config.js
@@ -20,9 +20,17 @@ module.exports = withImages(
       }),
     },
   })({
-    webpack: (config, { isServer }) => {
+    webpack: (config, { dev, isServer }) => {
       if (isServer) {
         require('./scripts/generate-sitemap');
+      }
+
+      if (!dev && !isServer) {
+        Object.assign(config.resolve.alias, {
+          react: 'preact/compat',
+          'react-dom/test-utils': 'preact/test-utils',
+          'react-dom': 'preact/compat',
+        });
       }
 
       return config;
