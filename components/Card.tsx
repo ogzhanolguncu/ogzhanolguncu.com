1f8bd442c2245b66d0a4c5c1962d3cf748b29325
ogzhanolguncu <ogzhan11@gmail.com>
2021-03-24 00:46:53 +0300

    fixed some styling issues and added cover image

--- components/Card.tsx
+++ components/Card.tsx
@@ -37,12 +37,14 @@ const Card = ({ id, img, title, description }: Props) => {
             fontSize={['1.1rem', '1.1rem', '1.5rem', '1.5rem']}
             fontWeight="600"
             lineHeight="1.9rem"
-            m={['.5rem 0 1.2rem', '.5rem 0 1rem', '1rem 0 1rem', '1rem 0 3rem']}
+            m={['.5rem 0 1.2rem', '.5rem 0 1rem', '.5rem 0 1rem', '.5rem 0 1rem']}
             color={colorMode === 'light' ? '#000' : '#fff'}
             height="96px"
             width="305px"
             textTransform="capitalize"
             letterSpacing="tight"
+            noOfLines={1}
+            isTruncated
           >
             {title}
           </Heading>
