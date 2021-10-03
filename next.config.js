fe305f2e63e3a1d91578d432bb4b814b8f61cbdc
ogzhanolguncu <ogzhan11@gmail.com>
2021-10-03 15:07:04 +0300

    Update sitemap

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
 module.exports = {
   webpack: (config, { dev, isServer }) => {
-    if (isServer) {
-      require('./scripts/generate-sitemap');
-    }
-
     if (!dev && !isServer) {
       Object.assign(config.resolve.alias, {
         react: 'preact/compat',
