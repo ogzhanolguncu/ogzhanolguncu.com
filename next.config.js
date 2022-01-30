af766c29df199a585987761657b9000a847aab00
ogzhanolguncu <ogzhan11@gmail.com>
2022-01-31 01:14:24 +0300

    Update blogpost

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
+/**
+ * @type {import('next').NextConfig}
+ */
 module.exports = {
+  swcMinify: true,
+  reactStrictMode: true,
+
   webpack: (config, { dev, isServer }) => {
+    // Replace React with Preact only in client production build
     if (!dev && !isServer) {
       Object.assign(config.resolve.alias, {
         react: 'preact/compat',
