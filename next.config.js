232e93b076720c0729daf72f487ffbce42ca0d0b
ogzhanolguncu <ogzhan11@gmail.com>
2022-01-31 10:56:39 +0300

    Update deps

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
  * @type {import('next').NextConfig}
  */
 module.exports = {
+  swcMinify: true,
+  reactStrictMode: true,
+
   webpack: (config, { dev, isServer }) => {
     // Replace React with Preact only in client production build
     if (!dev && !isServer) {
