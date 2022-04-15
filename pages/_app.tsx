623b41d0c983d5f75053bbe355a2d216384874b2
ogzhanolguncu <ogzhan11@gmail.com>
2022-04-16 01:45:24 +0300

    Add context back to fix pipeline error

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,13 +5,16 @@ import React from 'react';
 import { DefaultSeo } from 'next-seo';
 import SEO from 'next-seo.config';
 import { Chakra } from '@components/Chakra';
+import { CustomColorModeProvider } from '@contexts/CustomColorContext';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Chakra cookies={pageProps.cookies}>
       <MDXProvider components={MDXComponents}>
-        <DefaultSeo {...SEO} />
-        <Component {...pageProps} />
+        <CustomColorModeProvider>
+          <DefaultSeo {...SEO} />
+          <Component {...pageProps} />
+        </CustomColorModeProvider>
       </MDXProvider>
     </Chakra>
   );
