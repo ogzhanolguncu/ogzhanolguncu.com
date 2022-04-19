4507a022b496bf7593dde757704bf73ccf9daaf0
ogzhanolguncu <ogzhan11@gmail.com>
2022-04-19 22:57:30 +0300

    Add code section

--- componentsV2/Hero/HeroCode.tsx
+++ componentsV2/Hero/HeroCode.tsx
@@ -17,28 +17,56 @@ const HeroSectionCode = () => {
       alignItems={['flex-start', 'flex-start', 'flex-end', 'flex-end']}
     >
       <Box position="relative">
-        <TypeContainer>
+        <Box
+          w={['300px', '400px', '300px', '400px']}
+          height="450px"
+          borderRadius="9px"
+          background="black"
+          boxShadow="6px 6px #8080805e"
+          padding="2rem"
+        >
           <Box
-            w={['300px', '400px', '300px', '400px']}
-            height="450px"
-            borderRadius="9px"
-            background="black"
-            boxShadow="6px 6px #8080805e"
-            padding="2rem"
+            as="pre"
+            // ref={typeRef}
+            color="#fff"
+            // visibility="hidden"
+            fontSize={['1.2rem', '1.2rem', '1.5rem', '1.5rem']}
+            fontFamily="Cabin"
           >
-            <Box
-              as="pre"
-              ref={typeRef}
-              color="#fff"
-              visibility="hidden"
-              fontSize={['1.2rem', '1.2rem', '1.5rem', '1.5rem']}
-              fontFamily="Cabin"
-              whiteSpace="break-spaces"
-            >
-              {CODE_USER_INFO}
-            </Box>
+            {/* {CODE_USER_INFO} */}
+            <Flex>
+              <Text color="#c0ad60">const </Text> <Text>userInfo</Text> <Text>: {'{'} </Text>
+            </Flex>
+            <Flex ml="3.8rem">
+              <Text>name: </Text> &#39;<Text color="rgba(253, 149, 90, 0.8)">Oguzhan</Text>
+              &#39; ,
+            </Flex>
+            <Flex ml="3.8rem">
+              <Text>surname: </Text> &#39;<Text color="rgba(253, 149, 90, 0.8)">Olguncu</Text>
+              &#39; ,
+            </Flex>
+            <Flex ml="3.8rem">
+              <Text>location: </Text> &#39;
+              <Text color="rgba(253, 149, 90, 0.8)">Istanbul/Turkey</Text>
+              &#39; ,
+            </Flex>
+            <Flex ml="3.8rem">
+              <Text>technologies: </Text> <Text>{'['} </Text>
+            </Flex>
+            <Flex ml="1.5rem">
+              &#39;<Text color="rgba(253, 149, 90, 0.8)">Javascript</Text>&#39;,
+            </Flex>
+            <Flex ml="1.5rem">
+              &#39;<Text color="rgba(253, 149, 90, 0.8)">Typescript</Text>&#39;,
+            </Flex>
+            <Flex ml="1.5rem">
+              &#39;<Text color="rgba(253, 149, 90, 0.8)">React</Text>&#39;{']'}
+            </Flex>
+            <Flex>
+              <Text>{'};'}</Text>
+            </Flex>
           </Box>
-        </TypeContainer>
+        </Box>
         <Box
           position="absolute"
           left={['50px', '50px', '-80px', '-80px']}
