cfc1ad622a8d1a3250069a911cac4d8f227d4e89
Oguzhan Olguncu <oguzhan@fireflyon.com>
2022-04-22 12:08:56 +0300

    Add georgia as a fallback font

--- styles/theme.tsx
+++ styles/theme.tsx
@@ -2,8 +2,8 @@ import { extendTheme } from '@chakra-ui/react';
 
 const theme = extendTheme({
   fonts: {
-    body: `Cabin, sans-serif`,
-    heading: `Cabin, sans-serif`,
+    body: `Cabin, Georgia`,
+    heading: `Cabin, Georgia`,
   },
   fontWeights: {
     normal: 400,
