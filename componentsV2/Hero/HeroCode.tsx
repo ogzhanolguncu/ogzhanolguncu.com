10e6fa2f646e925324c99e390f21ba30ef5acd78
ogzhanolguncu <ogzhan11@gmail.com>
2023-01-08 21:02:09 +0300

    Fix missing page

--- componentsV2/Hero/HeroCode.tsx
+++ componentsV2/Hero/HeroCode.tsx
@@ -11,19 +11,19 @@ const HeroSectionCode = () => {
     <Flex
       w="100%"
       flexDirection="column"
-      justifyContent={['flex-start', 'flex-start', 'flex-start', 'flex-end']}
-      alignItems={['flex-start', 'flex-start', 'flex-start', 'flex-end']}
+      justifyContent={["flex-start", "flex-start", "flex-start", "flex-end"]}
+      alignItems={["flex-start", "flex-start", "flex-start", "flex-end"]}
     >
       <Box position="relative">
         <Box
-          w={['300px', '400px', '400px', '500px']}
+          w={["300px", "400px", "400px", "500px"]}
           height="450px"
           borderRadius="9px"
           background="black"
           boxShadow="8px 6px #8080805e"
           padding="2rem"
           color="#fff"
-          fontSize={['1.2rem', '1.5rem', '1.5rem', '1.5rem']}
+          fontSize={["1.2rem", "1.5rem", "1.5rem", "1.5rem"]}
         >
           <CodeContainer>
             <TypeIt
@@ -45,10 +45,10 @@ const HeroSectionCode = () => {
         </Box>
         <Box
           position="absolute"
-          left={['20px', '150px', '150px', '-70px']}
-          bottom={['-60px', '-60px', '-65px', '-70px']}
-          padding={['15px 20px', '15px 20px', '20px 30px', '20px 30px']}
-          w={['300px', '300px', '340px', '340px']}
+          left={["20px", "150px", "150px", "-70px"]}
+          bottom={["-60px", "-60px", "-65px", "-70px"]}
+          padding={["15px 20px", "15px 20px", "20px 30px", "20px 30px"]}
+          w={["300px", "300px", "340px", "340px"]}
           borderRadius="10px"
           background="rgba(255, 255, 255, 0.27)"
           boxShadow="0 4px 30px rgba(0, 0, 0, 0.1)"
@@ -56,13 +56,20 @@ const HeroSectionCode = () => {
         >
           <Flex>
             <Image
-              style={{ borderRadius: '50%' }}
-              src={'/static/images/colored.webp'}
-              alt="Profile Photo Oğuzhan Olguncu"
+              src={"/static/images/colored.webp"}
+              alt="Profile Photo of Oğuzhan Olguncu"
               loading="eager"
-              placeholder="empty"
+              placeholder="blur"
+              blurDataURL={`data:image/svg+xml;base64,${toBase64(
+                shimmer(700, 475)
+              )}`}
               width={90}
               height={90}
+              style={{
+                maxWidth: "100%",
+                height: "auto",
+                borderRadius: "50%",
+              }}
               priority
             />
             <Flex
@@ -85,3 +92,22 @@ const HeroSectionCode = () => {
 };
 
 export default HeroSectionCode;
+
+const shimmer = (w: number, h: number) => `
+<svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+  <defs>
+    <linearGradient id="g">
+      <stop stop-color="#333" offset="20%" />
+      <stop stop-color="#222" offset="50%" />
+      <stop stop-color="#333" offset="70%" />
+    </linearGradient>
+  </defs>
+  <rect width="${w}" height="${h}" fill="#333" />
+  <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
+  <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
+</svg>`;
+
+const toBase64 = (str: string) =>
+  typeof window === "undefined"
+    ? Buffer.from(str).toString("base64")
+    : window.btoa(str);
---
+++ pages/about.tsx
@@ -0,0 +1,116 @@
+import { Heading, Text, Flex, Box } from '@chakra-ui/react';
+
+import React, { ReactNode } from 'react';
+import { NextSeo } from 'next-seo';
+import Layout from 'componentsV2/Layout';
+
+const url = 'https://ogzhanolguncu.com/about';
+const title = 'About Me – Oğuzhan Olguncu';
+
+const CustomText = ({ children }: { children: ReactNode }) => {
+  return (
+    <Box
+      fontSize="18px"
+      lineHeight="1.7"
+      css={{
+        wordSpacing: '1.2px',
+        letterSpacing: '0.1px',
+      }}
+      fontWeight={500}
+    >
+      {children}
+    </Box>
+  );
+};
+const About = () => {
+  return (
+    <>
+      <NextSeo
+        title={title}
+        canonical={url}
+        openGraph={{
+          url,
+          title,
+        }}
+      />
+      <Layout>
+        <Flex
+          justifyContent="center"
+          alignItems="center"
+          margin="5rem 0"
+          flexDirection="column"
+          color="gray.800"
+        >
+          <Heading
+            as="h1"
+            fontSize={['2rem', '2rem', '3rem', '3rem']}
+            marginBottom="1rem"
+            fontWeight="bold"
+            lineHeight="1.4"
+          >
+            About me
+          </Heading>
+          <Flex
+            padding={['1rem', '2rem', '2rem', '2rem']}
+            marginTop="1.3rem"
+            display="flex"
+            flexDirection="row"
+            justifyContent="flex-start"
+            alignItems="flex-start"
+            w={['100%', '90%', '75%', '75%']}
+            flexWrap="wrap"
+            gap="1rem"
+          >
+            <CustomText>
+              Hi! I'm Oğuzhan Olguncu. I currently work full-time as a Software Engineer, and I
+              build
+              <Text as="span" backgroundColor="#000" color="#fff" p="0.2rem" mx="0.3rem">
+                open-source projects
+              </Text>
+              for fun. I taught myself how to make websites first as a hobby and later as a
+              professional programmer. I've been documenting everything I've learned since I began.
+              I also love to make open source projects, all of which can be found on Github and I'm
+              also huge classical Turkish music lover and recreational powerlifter.
+              <Heading fontSize="1.3rem" mt="3rem" mb="1rem">
+                Get in touch
+              </Heading>
+              I hope you love the site, and if there's anything you want to talk about with me feel
+              free to drop me a line by email. I'm happy to hear your comments, feedback,
+              suggestions, or just say hi!
+              <em>(Emails regarding ads, sponsored posts, link-sharing, etc. will be ignored.)</em>
+            </CustomText>
+            <Flex gap="2rem" mt="1rem">
+              <Box>
+                📧 &nbsp; <strong>Github:</strong> {''}
+                <Text
+                  as="a"
+                  href="https://github.com/ogzhanolguncu"
+                  backgroundColor="#000"
+                  color="#fff"
+                  p="0.2rem"
+                >
+                  @ogzhanolguncu
+                </Text>
+              </Box>
+              <Box>
+                🐙 &nbsp; <strong>Email:</strong> {''}
+                <Text
+                  as="a"
+                  href="mailto:ogzhan11@gmail.com"
+                  backgroundColor="#000"
+                  color="#fff"
+                  p="0.2rem"
+                >
+                  @ogzhanolguncu
+                </Text>
+              </Box>
+            </Flex>
+          </Flex>
+        </Flex>
+        <Box mt={['3rem', '3rem', '6rem', '6rem']} />
+      </Layout>
+    </>
+  );
+};
+
+export default About;
