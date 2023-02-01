96268bf2dbe773c5f0ecf56907b9a2d6ee1ece47
ogzhanolguncu <ogzhan11@gmail.com>
2023-02-02 00:22:59 +0300

    Update rss

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,29 @@
-import React from 'react';
-import { Box } from '@chakra-ui/react';
-import { InferGetStaticPropsType } from 'next';
+import React from "react";
+import { Box } from "@chakra-ui/react";
+import { InferGetStaticPropsType } from "next";
 
-import { getAllFilesFrontMatter } from 'lib/mdx';
+import { getAllFilesFrontMatter } from "lib/mdx";
 
-import ArticleList from 'componentsV2/Article/ArticleList';
-import Hero from 'componentsV2/Hero';
-import Layout from 'componentsV2/Layout';
-import Project from 'componentsV2/Projects';
+import ArticleList from "componentsV2/Article/ArticleList";
+import Hero from "componentsV2/Hero";
+import Layout from "componentsV2/Layout";
+import Project from "componentsV2/Projects";
+import { generateRssFeed } from "scripts/generate-rss";
 
 type Props = InferGetStaticPropsType<typeof getStaticProps>;
 
 const Home = ({ blogPosts, popularPosts }: Props) => {
   return (
     <Layout>
-      <Box mt={['5rem', '5rem', '8rem', '8rem']} />
+      <Box mt={["5rem", "5rem", "8rem", "8rem"]} />
       <Hero />
-      <Box mt={['6rem', '6rem', '12rem', '12rem']} />
+      <Box mt={["6rem", "6rem", "12rem", "12rem"]} />
       <ArticleList articles={blogPosts} showHeader limitCount />
-      <Box mt={['3rem', '3rem', '6rem', '6rem']} />
+      <Box mt={["3rem", "3rem", "6rem", "6rem"]} />
       <ArticleList articles={popularPosts} isPopular showHeader limitCount />
-      <Box mt={['6rem', '6rem', '12rem', '12rem']} />
+      <Box mt={["6rem", "6rem", "12rem", "12rem"]} />
       <Project />
-      <Box mt={['3rem', '3rem', '6rem', '6rem']} />
+      <Box mt={["3rem", "3rem", "6rem", "6rem"]} />
     </Layout>
   );
 };
@@ -30,8 +31,9 @@ const Home = ({ blogPosts, popularPosts }: Props) => {
 export default Home;
 
 export const getStaticProps = async () => {
-  const blogPosts = await getAllFilesFrontMatter('blog');
-
+  const blogPosts = await getAllFilesFrontMatter("blog");
+  await generateRssFeed(blogPosts);
+  
   const popularPosts = blogPosts.filter((blog) => blog.isPopular);
   return {
     props: { blogPosts, popularPosts },
